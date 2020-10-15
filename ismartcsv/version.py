996dc3ca921d5377c31c2eb841c68b12b0ed9ae8
jeldikk <jeldi.kamal2011@gmail.com>
2020-10-15 16:05:34 +0530

    added version.py for maintaing package versions

---
+++ ismartcsv/version.py
@@ -0,0 +1,11 @@
+"""
+central location for managings versions, change here and entire package version will be effected
+
+@author: jeldikk
+"""
+
+__title__ = 'ismartcsv'
+__author__ = "jeldikk"
+
+version_info = (1,0,7)
+__version__ = ".".join(map(str,version_info))
